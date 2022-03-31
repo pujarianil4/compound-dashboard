e65330a5233956b7089135952df4a7ec10953559
Anil Pujari <pujarianil4@gmail.com>
2022-03-31 17:56:21 +0530

    dashboard with apy values

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import logo from './logo.svg';
-import Compound from '@compound-finance/compound-js';
+import Compound, { util } from '@compound-finance/compound-js';
 import './App.css';
 import calculateApy from './apy';
 import React from 'react';
 
-const formatPercent = number =>
-`${new Number(number).toFixed(2)}%` 
+const formatPercent = (number) => {
+  console.log("number", number);
+  return `${ Number(number).toFixed(2)}%`;
+}
 
 function App() {
   const [apy, setApy] = React.useState([])
  
   React.useEffect(() => {
     (async () => {
-     const apy = await Promise.all([
-      calculateApy(Compound.cDAI, 'DAI'),
-      calculateApy(Compound.cUSDC, 'USDC'),
-      calculateApy(Compound.cUSDT, 'USDT'),
-    ])
-    console.log(apy);
-    setApy(apy)
+      try {
+        const apy = await Promise.all([
+          calculateApy(Compound.cDAI, 'DAI'),
+          calculateApy(Compound.cUSDC, 'USDC'),
+          calculateApy(Compound.cUSDT, 'USDT'),
+         // calculateApy(Compound.cAAVE, 'AAVE'),
+           calculateApy(Compound.cBAT, 'BAT'),
+          // calculateApy(Compound.cFEI, 'FEI'),
+          // calculateApy(Compound.cLINK, 'LINK'),
+          // calculateApy(Compound.cMKR, 'MKR'),
+           calculateApy(Compound.cREP, 'REP'),
+           calculateApy(Compound.cSAI, 'SAI'),
+          // calculateApy(Compound.cSUSHI, 'SUSHI'),
+          // calculateApy(Compound.cTUSD, 'TUSD'),
+           calculateApy(Compound.cUNI, 'UNI'),
+          // calculateApy(Compound.cUSDP, 'USDTP'),
+        ])
+        console.log(apy);
+        setApy(apy)
+      } catch (error) {
+         console.error("eRROR==>", error)
+      }
+    
     })()
-  })
+    console.log("compound", util);
+  }, [])
   return (
     <div className="App">
        <h1>Compound Dashboard</h1>
@@ -35,21 +54,31 @@ function App() {
           </tr>
         </thead>
         <tbody>
-         
-            <tr >
-              <td>
-                <img 
-                  src={`img/dai.png`}
-                  style={{width: 25, height:25, marginRight: 10}}
-                />
-                test
-              </td>
-              <td>tesr</td>
-              <td>test</td>
-              <td>test</td>
-            </tr>
+            {
+              apy && apy.map((apy)=> {
+                return (
+                  <tr key={apy.ticker} >
+                  <td>
+                    <div className='img_div'>
+                    <img 
+                      src={`img/${apy.ticker.toLowerCase()}.png`}
+                      style={{width: 25, height:25, marginRight: 10}}
+                    />
+                    <span> {apy.ticker}</span>
+                    </div>
+                  </td>
+                  <td>{formatPercent(apy.supplyApy)}</td>
+              <td>{formatPercent(apy.compApy)}</td>
+              <td>{formatPercent(parseFloat(apy.supplyApy) + parseFloat(apy.compApy))}</td>
+                </tr>
+                )
+              })
+            }
         </tbody>
       </table>
+      {
+        apy.length == 0 && <h4>Loading...</h4>
+      }
        </div>
     </div>
   );
