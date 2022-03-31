927847c2b60e4f31dc9783879f33229cf1efafbe
Anil Pujari <pujarianil4@gmail.com>
2022-03-31 16:17:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
+import Compound from '@compound-finance/compound-js';
 import './App.css';
+import calculateApy from './apy';
+import React from 'react';
+
+const formatPercent = number =>
+`${new Number(number).toFixed(2)}%` 
 
 function App() {
+  const [apy, setApy] = React.useState([])
+ 
+  React.useEffect(() => {
+    (async () => {
+     const apy = await Promise.all([
+      calculateApy(Compound.cDAI, 'DAI'),
+      calculateApy(Compound.cUSDC, 'USDC'),
+      calculateApy(Compound.cUSDT, 'USDT'),
+    ])
+    console.log(apy);
+    setApy(apy)
+    })()
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Compound Dashboard</h1>
+       <div>
+       <table className='table'>
+        <thead>
+          <tr> 
+            <th>Ticker</th>
+            <th>Supply APY</th>
+            <th>COMP APY</th>
+            <th>Total APY</th>
+          </tr>
+        </thead>
+        <tbody>
+         
+            <tr >
+              <td>
+                <img 
+                  src={`img/dai.png`}
+                  style={{width: 25, height:25, marginRight: 10}}
+                />
+                test
+              </td>
+              <td>tesr</td>
+              <td>test</td>
+              <td>test</td>
+            </tr>
+        </tbody>
+      </table>
+       </div>
     </div>
   );
 }
